d2250de05163fd063f6773dac3f3cc9430d2b97d
Juan Vilchez <juanvilchez2002@gmail.com>
2021-04-01 22:05:10 -0500

    Getters y setters

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -5,9 +5,24 @@ function MediaPlayer(config){
 };
 
 MediaPlayer.prototype._initPlugins = function(){
-    this.plugins.forEach(plugin => {
-        plugin.run(this);
-    });
+    // this.plugins.forEach(plugin => {
+    //     plugin.run(this);
+    // });
+
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+
+        media: this.media,
+
+        get muted(){
+            return this.media.muted
+        },
+
+        set muted(value){
+            this.media.muted = value
+        }
+    }
 }
 
 MediaPlayer.prototype.play = function(){
--- plugin/AutoPlay.js
+++ plugin/AutoPlay.js
@@ -3,7 +3,15 @@ function AutoPlay(){
 }
 
 AutoPlay.prototype.run = function(player){
-    player.mute();
-    player.play();
-}
+    // player.mute();
+    // player.play();
+
+    if(!player.muted){
+        player.muted = true
+    }else{     
+        player.muted = false
+    }
+
+    player.play()
+};
 export default AutoPlay;
