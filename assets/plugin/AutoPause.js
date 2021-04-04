6da6e871789f741ce72268b95ab176c406648fb0
Juan Vilchez <juanvilchez2002@gmail.com>
2021-04-03 22:25:02 -0500

    VisibilityChange

--- assets/plugin/AutoPause.js
+++ assets/plugin/AutoPause.js
@@ -3,6 +3,7 @@ class AutoPause{
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
     }
     run(player){
         this.player = player;
@@ -12,6 +13,9 @@ class AutoPause{
         });
 
         observer.observe(this.player.media);
+
+        document.addEventListener("visibilitychange",this.handleVisibilityChange)
+
     }
 
     handleIntersection(entries){
@@ -25,6 +29,15 @@ class AutoPause{
         }else{
             this.player.pause()
         }        
+    }
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState === 'visible'
+        if(isVisible){
+            this.player.play()
+        }else{
+            this.player.pause()
+        } 
     }
 }
 
